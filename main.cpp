6430f308a5597cf171592f9e034dd067aaac09b1
Milica Trifunovic <trifunoviceva97@gmail.com>
2020-09-09 09:59:26 +0200

    Hello push again Resolved

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main() {
 	std::cout << "Hello push" << std::endl;
+<<<<<<< HEAD
 	std::cout << "Hello push again" << std::endl;
+=======
+	std::cout << "Hello push once again" << std::endl;
+>>>>>>> 5c8d89a... Hello push again
     return 0;
 }
